379852623d6a0e6503cef63757877cfbd013c31a
beifengtz <beifengtz@qq.com>
2023-06-12 09:54:17 +0800

    feat 扩充util方法
    (cherry picked from commit 108a6fa060433ce60d4d5663dee4bd0c2db25c97)

--- common/src/main/java/org/beifengtz/jvmm/common/util/StringUtil.java
+++ common/src/main/java/org/beifengtz/jvmm/common/util/StringUtil.java
@@ -1,8 +1,12 @@
 package org.beifengtz.jvmm.common.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * <p>
@@ -14,6 +18,7 @@ import java.util.UUID;
  * @author beifengtz
  */
 public class StringUtil {
+    private static final Pattern emojiPattern = Pattern.compile("[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]");
 
     public static String genUUID(boolean upper) {
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
@@ -149,5 +154,105 @@ public class StringUtil {
             str.append(item);
         }
         return str.toString();
+    }
+
+
+    /**
+     * 过滤掉emoji
+     *
+     * @param str 字符源
+     * @return 过滤后的字符串
+     */
+    public static String filterEmoji(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            return str;
+        }
+        return emojiPattern.matcher(str).replaceAll("");
+    }
+
+    /**
+     * 判断是否包含emoji
+     *
+     * @param str 字符源
+     * @return true or false
+     */
+    public static boolean containsEmoji(String str) {
+        if (str == null || str.trim().isEmpty()) {
+            return false;
+        }
+        return emojiPattern.matcher(str).find();
+    }
+
+    /**
+     * 优雅地格式化字节数
+     *
+     * @param bytes 字节数
+     * @return 可视化的字节表示，比如：12.23MB, 1.7GB, 30KB, 120B
+     */
+    public static String formatByteSizeGracefully(long bytes) {
+        double value = 0;
+        String unit = "";
+        if (bytes < 1024) {
+            value = bytes;
+            unit = "B";
+        } else if (bytes < 1024 * 1024) {
+            value = bytes / 1024.0;
+            unit = "KB";
+        } else if (bytes < 1024 * 1024 * 1024) {
+            value = bytes / (1024 * 1024.0);
+            unit = "MB";
+        } else {
+            value = bytes / (1024 * 1024 * 1024.0);
+            unit = "GB";
+        }
+
+        return BigDecimal.valueOf(value).setScale(2, RoundingMode.FLOOR).doubleValue() + unit;
+    }
+
+    /**
+     * 驼峰转下划线
+     *
+     * @param str 需转换的驼峰字符串
+     * @return 下划线字符串
+     */
+    public static String hump2Line(String str) {
+        String s = str.replaceAll("[A-Z]", "_$0").toLowerCase();
+        if (s.startsWith("_")) {
+            return s.substring(1);
+        }
+        return s;
+    }
+
+    /**
+     * 下划线转驼峰
+     *
+     * @param str 需转换的下划线字符串
+     * @return 驼峰字符串
+     */
+    public static String line2Hump(String str) {
+        Pattern linePattern = Pattern.compile("_(\\w)");
+
+        str = str.toLowerCase();
+
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return upperCaseFirst(sb.toString());
+    }
+
+    /**
+     * 首字母转为大写
+     *
+     * @param s 字符源
+     * @return result
+     */
+    public static String upperCaseFirst(String s) {
+        if (Character.isUpperCase(s.charAt(0)))
+            return s;
+        else
+            return Character.toUpperCase(s.charAt(0)) + s.substring(1);
     }
 }
