c8b51e93e2f36ecd02f40eb18d5a2ae4fb698c2c
beifengtz <beifengtz@qq.com>
2023-06-28 20:43:04 +0800

    feat 优化Enhancer，支持根据 className 和 methodName创建单独的Listener

--- asm/src/test/java/org/beifengtz/jvmm/asm/test/People.java
+++ asm/src/test/java/org/beifengtz/jvmm/asm/test/People.java
@@ -1,7 +1,5 @@
 package org.beifengtz.jvmm.asm.test;
 
-import java.util.Random;
-
 /**
  * description: TODO
  * date: 18:14 2023/6/28
@@ -11,8 +9,19 @@ import java.util.Random;
 public class People {
     int counter;
 
-    public void say() throws Exception {
-        System.out.println("Say number " + counter++);
-        Thread.sleep(new Random().nextInt(3) * 1000);
+    public void say() {
+        System.out.println("Say " + counter++);
+    }
+
+    public void run() {
+        System.out.println("Run " + counter++);
+    }
+
+    public void sayHello() {
+        System.out.println("Say Hello " + counter++);
+    }
+
+    public void sayHelloWorld() {
+        System.out.println("Say Hello World! " + counter++);
     }
 }
